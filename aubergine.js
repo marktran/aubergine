54d939938b1712a160924aa74065faa3637cbb74
Mark Tran <mark.tran@gmail.com>
2010-07-17 14:34:33 -0700

    added ajax call, backend yelp request, changed to 2 space tabs, enabled logging, other refactoring

--- aubergine.js
+++ aubergine.js
@@ -1,37 +1,91 @@
 // aubergine.js : Mark Tran <mark@nirv.net>
-
 var express = require('express'),
+    fs = require('fs');
     connect = require('connect'),
+    http = require('http');
     sys = require('sys');
 
-var app = express.createServer();
+var app = express.createServer(
+  connect.logger()
+);
 
 // configuration
 app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.use('/', connect.bodyDecoder());
-    app.use('/', connect.methodOverride());
-    app.use('/', connect.compiler({ src: __dirname + '/public', enable: ['sass'] }));
-    app.use('/', connect.staticProvider(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.use('/', connect.bodyDecoder());
+  app.use('/', connect.methodOverride());
+  app.use('/', connect.compiler({ src: __dirname + '/public',
+                                  enable: ['sass'] }));
+  app.use('/', connect.staticProvider(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-    app.set('reload views', 1000);
-    app.use('/', connect.errorHandler({ dumpExceptions: true, showStack: true }));
-    app.use('/', connect.logger())
+  app.set('reload views', 1000);
+  app.use('/', connect.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-   app.use('/', connect.errorHandler()); 
+  app.use('/', connect.errorHandler());
 });
 
 // routes
 app.get('/', function(req, res) {
-    res.render('index.haml', {
-        locals: {
-            title: 'aubergine'
-        }
+  res.render('index.haml', {
+    locals: {
+      title: 'aubergine'
+    }
+  });
+});
+
+function getYelpData(latitude, longitude, fn) {
+  var client = http.createClient(80, 'api.yelp.com'),
+  req = client.request('GET',
+                       '/business_review_search?term=restaurants&lat=' +
+                       latitude +
+                       '&long=' +
+                       longitude +
+                       '&radius=0.5&limit=20&ywsid=XXX',
+                       { host: 'api.yelp.com' })
+  req.addListener('response', function(res){
+    res.body = '';
+    res.addListener('data', function(chunk) {
+      res.body += chunk;
     });
+    res.addListener('end', function(){
+      try {
+        fn(JSON.parse(res.body));
+      } catch (err) {
+        fn(err);
+      }
+    });
+  });
+  req.end();
+}
+
+function getLocalYelpData(latitude, longitude, fn) {
+  var data = JSON.parse(fs.readFileSync(__dirname + '/private/yelp.json',
+                                        'utf8'));
+  fn(data);
+}
+
+app.get('/ajax/yelp/locations', function(req, res) {
+  var latitude, longitude, locations = [];
+
+  latitude = req.params.get.latitude;
+  longitude = req.params.get.longitude;
+
+  getLocalYelpData(latitude, longitude, function(data) {
+    for (item in data.businesses) {
+      business = data.businesses[item];
+      locations.push({
+        name: business.name,
+        latitude: business.latitude,
+        longitude: business.longitude,
+        rating: business.avg_rating,
+      });
+    }
+    res.send({ items: locations });
+  });
 });
 
 app.listen(3000);
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,48 +1,92 @@
 function initialize() {
-    var browserSupportFlag, initialLocation, map, marker;
+  var latitude, longitude;
 
-    map = new google.maps.Map(document.getElementById("map_canvas"), {
-        mapTypeControl: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoom: 15
-    });
-    
-    // try W3C Geolocation
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            initialLocation = new google.maps.LatLng(position.coords.latitude,
-                                                     position.coords.longitude);
-            map.setCenter(initialLocation);
+  map = new google.maps.Map(document.getElementById("map_canvas"), {
+    mapTypeControl: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoom: 16,
+  });
 
-            marker = new google.maps.Marker({
-                position: initialLocation, 
-                map: map, 
-                title: "W3C Geolocation",
-            });
-            attachInfoWindow(marker, marker.title);
-        }, function() {
-            handleNoGeolocation();
+  // try W3C Geolocation
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      setupMarker("W3C Geolocation",
+                  latitude,
+                  longitude,
+                  false,
+                  true);
+
+      $.getJSON("/ajax/yelp/locations", {
+        latitude: latitude,
+        longitude: longitude,
+      }, function(data) {
+        $.each(data.items, function(index, location) {
+          setupMarker(location.name,
+                      location.latitude,
+                      location.longitude,
+                      location.rating);
         });
-    } else {
-        handleNoGeolocation();
-    }
+      });
+    }, function() {
+      handleNoGeolocation();
+    });
+  } else {
+    handleNoGeolocation();
+  }
 }
 
-function attachInfoWindow(marker, contentString) {
-    var infowindow = new google.maps.InfoWindow({
-        content: "<p>" + marker.title + "</p>"
-    });
+function setupMarker(title, latitude, longitude, rating, center) {
+  var infowindow, marker, position, rating_hash;
 
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(marker.map, marker);
-    });
+  rating_hash = {
+    0: 'green',
+    1: 'blue',
+    2: 'purple',
+    3: 'orange',
+    4: 'yellow',
+    5: 'red',
+  }
+
+  position = new google.maps.LatLng(latitude, longitude);
+  marker = new google.maps.Marker({
+    icon: 'http://maps.google.com/mapfiles/ms/icons/' +
+          rating_hash[Math.floor(rating)] +
+          '-dot.png',
+    map: map,
+    position: position,
+    title: title,
+  });
+
+  // center marker if last parameter is true
+  if (center !== undefined) {
+    map.setCenter(position);
+  }
+
+  // add listener for InfoWindow
+  infowindow = new google.maps.InfoWindow({
+    content: "<p>" + marker.title + "</p>",
+    state: 0,
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    if (infowindow.state) {
+      infowindow.close(marker.map, marker);
+      infowindow.state = 0;
+    } else {
+      infowindow.open(marker.map, marker);
+      infowindow.state = 1;
+    }
+  });
+
+  return marker, infowindow;
 }
 
 function handleNoGeolocation() {
-    // default to San Francisco
-    initialLocation = new google.maps.LatLng(37, 122);
-    map.setCenter(initialLocation);
-    infowindow.setContent("Error: The Geolocation service failed.");
-    infowindow.setPosition(initialLocation);
-    infowindow.open(map);
+  var marker, infowindow;
+
+  // default to San Francisco
+  marker, infowindow = setupMarker("San Franciso", 37, -122, false, true);
+  infowindow.setContent("Error: The Geolocation service failed.");
+  infowindow.open(marker.map);
 }
